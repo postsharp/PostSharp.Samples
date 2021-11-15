37d5aad47f78a5fca73e8464754b44060fb18273
Antonín Procházka <antonin@postsharp.net>
2021-11-15 17:24:30 +0100

    #29346 NuGet dependencies updated.

--- Blazor/PostSharp.Samples.Blazor.AutoRetry/Services/WeatherService.cs
+++ Blazor/PostSharp.Samples.Blazor.AutoRetry/Services/WeatherService.cs
@@ -1,52 +1,29 @@
 ﻿using PostSharp.Samples.AutoRetry.Aspects;
-using System;
-using System.Net;
-using System.Net.Http;
+using PostSharp.Samples.Blazor.AutoRetry.Model;
 using System.Net.Http.Json;
-using System.Threading.Tasks;
 
 namespace PostSharp.Samples.Blazor.AutoRetry.Services
 {
   [AutoRetry]
   public class WeatherService
   {
-    private int counter;
-    private HttpClient httpClient;
+    private static int counter;
+    private readonly HttpClient httpClient;
 
     public WeatherService(HttpClient httpClient)
     {
       this.httpClient = httpClient;
     }
 
-    public async Task<WeatherForecast[]> GetCurrentForecast()
+    public async Task<WeatherForecast[]?> GetCurrentForecast()
     {
       // Fail every other request.
       if (++counter % 2 == 1)
       {
-        throw new WebException("Service unavailable.");
+        throw new HttpRequestException("Service unavailable.");
       }
 
       return await this.httpClient.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
     }
-  }
-
-  public class WeatherForecast
-  {
-    public DateTime Date
-    {
-      get; set;
-    }
-
-    public int TemperatureC
-    {
-      get; set;
-    }
-
-    public string Summary
-    {
-      get; set;
-    }
-
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
   }
 }
